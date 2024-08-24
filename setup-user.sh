0a91674dadf63d64fecbbe8554a281b37002189d
Muhammad Naqdi <didi@debian>
2024-08-24 20:27:16 +0330

    Update

--- setup-user.sh
+++ setup-user.sh
@@ -1,9 +1,10 @@
-mkdir -p ~/backgrounds ~/gits ~/keys ~/scripts ~/tmp ~/.config/alacritty ~/binary ~/projects
-
+mkdir -p ~/backgrounds ~/gits ~/keys ~/scripts ~/tmp ~/.config/alacritty ~/.xmonad
 cp -f vimrc ~/.vimrc
 cp -f init.el ~/.emacs
 cp -f alacritty.yml ~/.config/alacritty/alacritty.yml
 cp -f scripts/* ~/scripts/
+cp -f xmobarrc ~/.xmobarrc
+cp -f xmonad.hs ~/.xmonad/xmonad.hs
 
 if [[ -z $(grep '# user' ~/.profile) ]]
 then
